6e6fedb3ca3dcf4aa61b1e7a4393f7b362ca8088
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-05 17:58:20 +0200

    fix: using less agressive transform for compression

--- apps/Compression/include/hyperparameters.hpp
+++ apps/Compression/include/hyperparameters.hpp
@@ -5,5 +5,5 @@ namespace compression {
     const double R = 8.;
     const double c = 8.5; // exponent
     const double f = 8.;   // mantissa
-    const auto tr = TRANSFORM_MATRICES::DAUBECHIES_D40;
+    const auto tr = TRANSFORM_MATRICES::DAUBECHIES_D10;
 };
