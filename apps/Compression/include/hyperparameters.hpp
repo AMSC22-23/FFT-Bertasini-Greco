d4ae5b41e3ca820289130aba3e8ca837b642df27
Aurora Bertasini <mariaaurorabertasini@gmail.com>
2024-06-06 22:11:21 +0200

    fix: changed the default DWT for compression

--- apps/Compression/include/hyperparameters.hpp
+++ apps/Compression/include/hyperparameters.hpp
@@ -7,5 +7,5 @@ namespace compression {
     const Typedefs::DType R = 8.;
     const Typedefs::DType c = 8.5; // exponent
     const Typedefs::DType f = 8.;   // mantissa
-    const auto tr_mat = TRANSFORM_MATRICES::DAUBECHIES_D10;
+    const auto tr_mat = TRANSFORM_MATRICES::DAUBECHIES_D20;
 };
